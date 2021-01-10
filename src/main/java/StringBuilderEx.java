ff2c61cd112de5e11e75750370c74166674f1ec2
Feruza Hoja <feruza06.02@gmail.com>
2021-01-09 20:36:27 -0800

    Jan9

---
+++ src/main/java/StringBuilderEx.java
@@ -0,0 +1,71 @@
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class StringBuilderEx {
+
+    public void main(String[] args){
+    StringBuilder builder = new StringBuilder();
+    StringBuilder builder2 = new StringBuilder(574);
+    StringBuilder builder1 = new StringBuilder("Hi");
+    // by default capacity of S Builder is 16 if no argument given
+        // capacity method
+    assertThat(builder.capacity(), is(16));
+    assertThat(builder2.capacity(), is(574));
+
+    System.out.println("Contents of StringBuilder :" + builder);
+
+    // to size StringBuilder's capacity after construction use ensureCapacity()
+    builder.ensureCapacity(600);
+    //trimToSize()
+    builder.trimToSize();
+    //append
+        builder.append(1);
+        assertThat(builder.toString(),is("1"));
+
+        //to insert
+        builder.insert(1, 2);//wn inserting char - specify index, offset of char arry, and numb of charecters to copy from char array
+
+
+        char[] characAr ={'.','a','b','c'};
+        StringBuilder builderChar = new StringBuilder("abcd");
+        builder.insert(2, characAr,3,4);
+
+        assertThat(builder.toString(), is("abcd"));
+        //delete- start index and end index
+
+        builder.delete(2, 4);
+        //delete- a specif character
+        builder.deleteCharAt(2);
+
+
+        builder.replace(0, 4, "12356");
+        assertThat(builder.toString(), is("12578"));
+
+        //replace individul characters using setCharAt using
+
+        StringBuilder builder3 = new StringBuilder("48");
+        builder.setCharAt(4, 'g');
+        assertThat(builder.toString(), is("bkglb;l"));
+
+        //reverse strings
+
+        assertThat(builder.reverse().toString(),is("87541"));
+
+        //substrings - returns A String from a start index to an end index
+
+        assertThat(builder.substring(3,7), is("34567"));
+        //from a start index to the end of the string
+        assertThat(builder.substring(3), is("3456"));
+
+
+
+
+
+
+
+
+
+}}
---
+++ src/main/java/StringExAndMethods.java
@@ -0,0 +1,169 @@
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class StringExAndMethods {
+    public static void main(String[] args) {
+
+        String asString = "abcdefg";
+        asString.charAt(2); //prints the index letter that we specify
+        //System.out.println(asString.charAt(2));
+        //System.out.println("\t, \b, \n, \r, \', \", \\ ");
+
+
+        //ex1   //convert from int to Srting
+        Integer integer = 1;
+        integer.toString();
+        System.out.println(integer.toString());//1
+        //print int
+        System.out.println(integer);
+//ex 2
+        int value = 1234;
+        String string = String.valueOf(value);
+        System.out.println(string); // 1234
+
+//ex 3 convert from Str to Integer
+        String string1 = "123";
+        Integer fromStrToint = Integer.parseInt(string1);
+        System.out.println(fromStrToint);
+
+
+        //from Str to Int /float = valueOf()
+//ex4
+        String inttoStr = String.valueOf(2);
+
+        //ex5     //convert from Str to Array
+        char arrayfromDtr[] = string1.toCharArray();
+        System.out.println(arrayfromDtr);//123
+
+        // ex6 from String to char array
+        char[] cArray = {4, 9, 8};
+       // assertThat("498".toCharArray(), is(cArray));
+
+        System.out.println(string1.getBytes());
+
+        //we can create new string and assert
+        String empty = new String();
+        assertThat(empty.length(), is(0));
+        assertThat(empty.isEmpty(), is(true));
+
+        //we can create new string  with argumnts from char
+        char[] ch = {'2', '3'};
+        ch.toString();
+        assertThat(new String(ch), is("23"));
+
+        //compareTo methods of Strings
+        String str2 = "Hello";
+        assertThat(str2.compareTo("Hello"), is(0));
+        assertThat(str2.compareTo("Hello") < 0, is(true));
+        assertThat(str2.compareToIgnoreCase("Hello"), is(0));
+
+        //case is important wh using contains
+        assertThat(str2.contains("H"), is(true));
+        //contentEquals retuns true if String has same parameter as
+
+        assertThat(str2.contentEquals("Hello"), is(true));
+        //endsWith compares then end of the Strinf witht he parameter
+        assertThat(str2.endsWith("Hello"), is(true));
+
+        // empty
+        // regionMatches allows to compare part of string with part of another string
+        // regionMatches(boolean)
+//regionMatches(int toOffset, String )
+
+
+
+//    str2.regionMatches(2, 2)
+            //indexOf
+            assertThat(str2.indexOf("1"), is(2));
+            assertThat(str2.lastIndexOf("1"), is(9));
+            //substring
+            assertThat(str2.substring(2), is("1"));
+            //replace
+            str2.replace("e", "l");
+            //replaceFisrt
+            //toUpperCase
+            System.out.println(str2.toUpperCase());
+
+            System.out.println(str2.indexOf(2));
+
+
+            //trim
+            String padded = "       trim me  ";
+            String trimmed = padded.trim();
+            assertThat(padded.length(), is(15));
+            assertThat(trimmed, is("trim me"));
+            str2.trim();
+
+
+            //substring
+            String digit = "123456789";
+            assertThat(digit.substring(2), is("5678"));
+
+
+            //format- to construc strings
+        //format
+        //instead of % name will be printed
+        String name = "Caleb";
+        System.out.println(String.format("Hello %s", name));
+        assertThat("Hello %s", is("Hello Caleb"));
+
+
+        int value1 = 4;
+            String output = "The value " + value + " was used";
+            assertThat(output, is("The value 4 was used"));
+
+            String template = "The value %d was used";
+            String formatted = String.format(template, value1);
+            assertThat(String.format(str2, "Bod", "ran", 5, "circles"),is("Hello") );
+            assertThat(formatted, is("The value 4 was used"));
+
+            //placeholders
+            String use = "%s %s towards %d large %s";
+            assertThat(String.format(use, "Bob", "ran", 6, "onions"), is("Bob ran towards 6 large onions"));
+            System.out.println("Format:" + use);
+
+//splt - converst string into array
+
+        String splitNum = "123547895";
+        String[]result = splitNum.split("");
+        assertThat(result[0], is("0"));
+        assertThat(result.length, is(10));
+
+
+
+        }
+
+        @Test
+        public void regionMatches () {
+/* Parameters
+Here is the detail of parameters −
+toffset − the starting offset of the subregion in this string.
+other − the string argument.
+ooffset − the starting offset of the subregion in the string argument.
+len − the number of characters to compare.*/
+
+
+//        String Str1 = new String("Welcome to Tutorialspoint.com");
+//        String Str2 = new String("Tutorials");
+//        String Str3 = new String("TUTORIALS");
+//
+//        System.out.print("Return Value :" );
+//        System.out.println(Str1.regionMatches(11, Str2, 0, 9));
+//
+//        System.out.print("Return Value :" );
+//        System.out.println(Str1.regionMatches(11, Str3, 0, 9));
+
+
+            String x = "Hello";
+            String y = "Jello";
+            System.out.println(x.regionMatches(2, y, 2, 2));
+
+        }
+    }
+
+
+
+
+
