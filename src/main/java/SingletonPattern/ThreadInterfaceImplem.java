cf2907d8560d6d73bd64db060a018019c5acce52
Your Name <feruza06.02@gmail.com>
2021-02-06 11:51:16 -0800

    Threadtypes

---
+++ src/main/java/SingletonPattern/ThreadInterfaceImplem.java
@@ -0,0 +1,18 @@
+package SingletonPattern;
+
+public class ThreadInterfaceImplem {
+    public static class MyRunnable implements Runnable{
+
+        @Override
+        public void run() {
+            System.out.println("My runnable running");
+            System.out.println("My runnable finished");
+        }
+    }
+
+    public static void main(String[]args){
+
+        Thread myThread = new Thread(new MyRunnable() );
+        myThread.start();
+    }
+}
