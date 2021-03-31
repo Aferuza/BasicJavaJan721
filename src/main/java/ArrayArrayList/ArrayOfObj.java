3482cf542c6280625c3603135919d68615776636
Your Name <feruza06.02@gmail.com>
2021-03-30 23:46:21 -0700

    App contacts

---
+++ src/main/java/ArrayArrayList/ArrayOfObj.java
@@ -0,0 +1,22 @@
+package ArrayArrayList;
+
+import Inheritance.Person;
+
+public class ArrayOfObj {
+
+    public static void main(String []args){
+
+        Person p1 = new Person();
+        Person p2 = new Person();
+        Person p3= new Person();
+        Person p4 = new Person();
+
+        Person [] person = {p1,p2,p3, p4};
+        for(Person i: person){
+            //int temp = person[i];
+            System.out.println("Persons are " + person());
+
+        }
+
+    }
+}
